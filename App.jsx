adfb000846d6aed167a0636db9995e6cb9ddf71b
Kumar Atul munesh <131685933+atulmunesh07@users.noreply.github.com>
2025-04-03 16:53:14 +0530

    card-hover-effects

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+const App = () => {
+  return<div className='card'>
+    <div className='Container'>
+      <img src="/images/image.png" alt="" className='image-one' />
+    </div>
+    <div className='text-container'>
+      <h1>Ktm Duke 390</h1>
+      <p>Price   : ₹ 2,95,000</p>
+      <p>The KTM 390 Duke has received a Rs 18,000 discount that is effective immediately. Following the discount, the 390 Duke is now priced at Rs 2.95 lakh, ex-showroom.</p>
+
+    </div>
+
+  </div>
+}
+export default App;
